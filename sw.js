8b4e2c537f8822111f63e213b81bc1e31fca7d37
douglasfdias <douglasdias.dfd@gmail.com>
2026-04-02 22:04:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // v3 — start_url corrigido para /minutinhos/
 // ================================================================
 
-const CACHE_NAME = 'minutinhos-v5';
+const CACHE_NAME = 'minutinhos-v6';
 
 const ASSETS_TO_CACHE = [
   '/minutinhos/',
