7d0c1dfcd98940f78e77350adf29c4452be15d64
douglasfdias <douglasdias.dfd@gmail.com>
2026-07-11 14:04:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'minutinhos-v26';
+const CACHE_NAME = 'minutinhos-v27';
 
 const ASSETS_TO_CACHE = [
   '/minutinhos/',
